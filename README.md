2ea6d33c27b0180a8dfaf48564e9d0fa5b772f9c
ShengMin Zhang <me@shengmin.me>
2012-09-15 21:18:21 -0400

    Set up project

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-resume
-======
-
 Personal Resume
---
+++ src/scala/me/shengmin/resume/ResumeBuilder.scala
@@ -0,0 +1,12 @@
+package me.shengmin.resume
+
+/**
+ * Build resume using FreeMarker template
+ *
+ * @author ShengMin Zhang
+ */
+object ResumeBuilder {
+  def main(args : Array[String]) = {
+    
+  }
+}
